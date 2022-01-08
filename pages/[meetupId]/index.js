691e55741768ec9d81064db8a5db564956e6cda8
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-07 23:21:39 -0500

    Working With Dynamic Path Params In getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,30 @@
-import { Fragment } from "react";
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MettupDetails() {
+function MettupDetails(context) {
   return (
       <MeetupDetail 
         image="https://techcrunch.com/wp-content/uploads/2017/02/meetup-logo-script-1200x630.png"
         title="A first meetup"
         address="Some address 5, 123456 Some City"
-        description="description"
+        description="A first meetup description"
       />
   );
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image: '',
+        id: '',
+        title: '',
+        address: '',
+        description: ''
+      }
+    }
+  }
 }
 
 export default MettupDetails;
