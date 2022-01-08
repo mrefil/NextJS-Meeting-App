cc8c3582fe900d4807eb88169e4a9a49dce265f4
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-07 23:37:24 -0500

    Connecting & Querying a MongoDB Database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,21 @@
-function handler(req, res) {
-    if(req.method === 'POST') {
-        const data = req.body;
+import { MongoClient } from "mongodb";
 
-        const { title, image, address, description } = data;
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    // const { title, image, address, description } = data;
     
-    }
+    //mongo db connection
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clusterID}.agazq.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+    const client = await MongoClient.connect(connectionString);
+    const db = client.db();
+
+    const meetupsCollection = db.collection(process.env.mongodb_database); 
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({message: 'Successfully submitted!'});
+  }
 }
 
 export default handler;
