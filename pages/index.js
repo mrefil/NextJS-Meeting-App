64d6b147d00741c8a879303c3a87d20c78ed40a4
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-07 22:54:41 -0500

    The "_app.js" File & Wrapper Components

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://techcrunch.com/wp-content/uploads/2017/02/meetup-logo-script-1200x630.png',
-        address: 'Some address 5, 123456 Some City',
-        description: 'This is a first meetup'
-    }
-]
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://techcrunch.com/wp-content/uploads/2017/02/meetup-logo-script-1200x630.png",
+    address: "Some address 5, 123456 Some City",
+    description: "This is a first meetup",
+  },
+];
 
 function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  );
 }
 
 export default HomePage;
